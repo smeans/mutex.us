73864398f151cb939398927ea276d874c698a9f5
Scott Means <smeans@gmail.com>
2022-06-18 14:44:46 +0700

    first pass at unit tests

--- server/main_test.go
+++ server/main_test.go
@@ -5,9 +5,14 @@ import (
     "testing"
     "net/http"
     "io/ioutil"
+    "time"
+    "log"
+    "encoding/json"
 )
 
 var baseURL string
+var testEmail string
+var clientID string
 
 func init() {
     if *AddrTLS != "" {
@@ -15,6 +20,10 @@ func init() {
     } else {
         baseURL = "http://" + *Addr
     }
+
+    testEmail = fmt.Sprintf("test-%d@mutex.us", time.Now().Unix())
+
+    log.Printf("mutex.us unit test initialized")
 }
 
 func TestRegister(t *testing.T) {
@@ -27,12 +36,66 @@ func TestRegister(t *testing.T) {
                 res.StatusCode, body)
     }
 
-    registerURL := fmt.Sprintf("%s/api/client?register&email=test@mutex.us", baseURL)
+    registerURL := fmt.Sprintf("%s/api/client?register&email=%s", baseURL, testEmail)
     res, _ = http.PostForm(registerURL, nil)
 
     body, _ = ioutil.ReadAll(res.Body)
     if res.StatusCode != 200 {
+        bodyText := string(body)
         t.Errorf("POST %s: expected 200: received: %d\n%s", clientURL,
-                res.StatusCode, body)
+                res.StatusCode, bodyText)
+        return
+    }
+
+    var clientInfo ClientInfo
+
+    err := json.Unmarshal(body, &clientInfo)
+    if err != nil {
+        t.Errorf("TestRegister: unable to unmarshal ClientInfo")
+        return
+    }
+
+    clientID = clientInfo.ClientID
+}
+
+func TestLock(t *testing.T) {
+    mutexURL := fmt.Sprintf("%s/api/client/%s/mutex/testmutex", baseURL,
+            clientID)
+
+    lockURL := fmt.Sprintf("%s?lock&waitTimeoutMs=3000", mutexURL)
+    res, _ := http.PostForm(lockURL, nil)
+
+    body, _ := ioutil.ReadAll(res.Body)
+    bodyText := string(body)
+    if res.StatusCode != 200 {
+        t.Errorf("POST %s: expected 200: received: %d\n%s", lockURL,
+                res.StatusCode, bodyText)
+    }
+
+    res, _ = http.PostForm(lockURL, nil)
+
+    body, _ = ioutil.ReadAll(res.Body)
+    if res.StatusCode != 409 {
+        t.Errorf("POST %s: expected 400: received: %d\n%s", lockURL,
+                res.StatusCode, bodyText)
+    }
+
+    unlockURL := fmt.Sprintf("%s?unlock", mutexURL)
+    res, _ = http.PostForm(unlockURL, nil)
+
+    body, _ = ioutil.ReadAll(res.Body)
+    bodyText = string(body)
+    if res.StatusCode != 200 {
+        t.Errorf("POST %s: expected 200: received: %d\n%s", unlockURL,
+                res.StatusCode, bodyText)
+    }
+
+    res, _ = http.PostForm(unlockURL, nil)
+
+    body, _ = ioutil.ReadAll(res.Body)
+    bodyText = string(body)
+    if res.StatusCode != 409 {
+        t.Errorf("POST %s: expected 409: received: %d\n%s", unlockURL,
+                res.StatusCode, bodyText)
     }
 }
